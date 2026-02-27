15287f95155cf3960f1e48c4acb0dda36a432343
Luis M <77978673+OrangeXR@users.noreply.github.com>
2026-02-27 01:09:44 -0600

    .

--- A/assets/js/main.js
+++
@@ -1,8 +0,0 @@
-// Video thumbnail click handler
-document.querySelectorAll(".thumb").forEach(t => {
-  t.addEventListener("click", () => {
-    const id = t.getAttribute("data-video");
-    document.getElementById("mainPlayer").src =
-      `https://www.youtube.com/embed/${id}`;
-  });
-});
--- B/assets/js/main.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll(".thumb").forEach(t => {
-  t.addEventListener("click", () => {
-    const id = t.getAttribute("data-video");
-    document.getElementById("mainPlayer").src =
-      `https://www.youtube.com/embed/${id}`;
-  });
-});
